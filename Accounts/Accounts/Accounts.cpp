20d4d597332fca4b9ae981dae21fdd3346da899b
qod8bop <turcenkovtimofej99228@gmail.com>
2025-06-05 23:08:53 +0300

    PLEASEWORK

---
+++ Accounts/Accounts/Accounts.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Accs {
+    int Number;
+    string Owner;
+    double balance;
+
+    void show() {
+        cout << "Ваш счёт: " << Number << " , " << Owner << " , " << balance;
+    }
+};
+
+void balchange(Accs* account, double newsum);
+
+void Accreate(Accs* account);
+
+int main() {
+
+    double newsum;
+    Accs account;
+
+    Accreate(&account);
+    cout << "Введите новую сумму: ";
+    cin >> newsum;
+    cout << endl;
+    balchange(&account, newsum);
+    account.show();
+
+    return 0;
+}
+
+void Accreate(Accs* account) {
+    cout << "Введите номер счёта: ";
+    cin >> account->Number;
+    cout << endl;
+
+    cout << "Введите имя владельца: ";
+    cin >> account->Owner;
+    cout << endl;
+
+    cout << "Введите баланс: ";
+    cin >> account->balance;
+    cout << endl;
+}
+
+void balchange(Accs* account, double newsum) {
+
+    account->balance = newsum;
+}   
---
+++ Months/Months/Months.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+
+using namespace std;
+
+enum  months {
+    January = 1, February, March, April, May, June,
+    July, August, September, October, November, December
+};
+
+int monthask() {
+
+    int input;
+
+    cout << "Введите номер месяца: ";
+    cin >> input;
+
+    if (input == 0) {
+        return 0;
+    }
+
+    if (input < 1 || input > 12) {
+        return 1;
+    }
+
+    months output = static_cast<months>(input);
+    switch (output) {
+    case January:
+        cout << "Январь" << endl;
+        break;
+    case February:
+        cout << "Февраль" << endl;
+        break;
+    case March:
+        cout << "Март" << endl;
+        break;
+    case April:
+        cout << "Апрель" << endl;
+        break;
+    case May:
+        cout << "Май" << endl;
+        break;
+    case June:
+        cout << "Июнь" << endl;
+        break;
+    case July:
+        cout << "Июль" << endl;
+        break;
+    case August:
+        cout << "Август" << endl;
+        break;
+    case September:
+        cout << "Сентябрь" << endl;
+        break;
+    case October:
+        cout << "Октябрь" << endl;
+        break;
+    case November:
+        cout << "Ноябрь" << endl;
+        break;
+    case December:
+        cout << "Декабрь" << endl;
+        break;
+    }
+    return 2;
+}
+
+
+
+int main() {
+    int yashatal;
+
+    while (true) {
+
+        yashatal = monthask();
+
+        if (yashatal == 0) {
+            cout << "До свидания !";
+            break;
+        }
+        if (yashatal == 1) {
+            cout << "Неправильный номер!" << endl;
+        }
+
+
+    }
+
+    
+
+    return 0;
+}
